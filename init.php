983ab1f4260ad6631ef35c5476c30a69a9d2be3f
Андрей <andrey.buharov@gmail.com>
2026-05-28 00:25:41 +0300

    Автокоммит изменений в events

--- init.php
+++ init.php
@@ -1,2 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 defined('EVENTS_VERSION') OR define('EVENTS_VERSION', '2.0.0');
+
+Kohana::$config->load('menu')
+    ->set('event', array(
+        'title' => 'События',
+        'url' => 'event',
+        'icon' => 'fa-cog',
+        'order' => 20,
+       
+    ));
